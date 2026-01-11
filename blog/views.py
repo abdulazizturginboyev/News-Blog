5c206fcc16e206a9bde7e20196e7ef987f84b9c3
abdulazizturginboyev <abdulazizturginboyevusa@gmail.com>
2026-01-11 20:13:30 +0500

    Delete, add , change news
    I add new delete function and add funtion on user side not only in admin panel it is very useful

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView ,DetailView
 from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 class PostListView(ListView):
@@ -10,3 +12,18 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title','author','body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
